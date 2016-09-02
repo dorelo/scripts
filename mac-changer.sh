c9f696f10bc1c12c7b8d6e4c43d9185f6490b8b9
dorelo <david@no-email.com>
2016-09-02 16:55:57 +0100

    Add mac-changer.sh

---
+++ mac-changer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+CURRENT="$(ifconfig en0 |grep ether)"
+echo "Current MAC : ${CURRENT}"
+/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -z
+ifconfig en0 ether $(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/./0/2; s/.$//')
+networksetup -detectnewhardware
+NEW="$(ifconfig en0 |grep ether)"
+echo "New MAC     : ${NEW}"
